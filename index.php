27022b1a3ea92938b71dc434dd13353cab69c28d
plats16 <46740637+plats16@users.noreply.github.com>
2020-05-14 20:45:40 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en"> 
+
+<?php include 'pages/header.php'; ?>  
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="backend/css/bootstrap.min.css" rel="stylesheet">
+  <link href="backend/css/main.css" rel="stylesheet"> 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+
+
+
+<!-- Page Content --> 
+
+ <section class="notes">
+    <div class="container">
+    Hi guys! This site was created just for you. <b>It's a very straight to the point site and simple</b>. You can <b>download</b> high quality pictures, poems, thoughts and sayings that are absolutely free(no copyright issues). Hope you like my monochromatic theme. Thanks!
+	  
+    </div>
+  </section> 
+  
+  <section class="responsive-images-columns">    
+    <?php $page = 'home'; include 'pages/mainheader.php';?>  
+    <div class="container"> 
+      <img class="breakpoint"  src="images/annie-spratt-jLQvbLPyenU-unsplash.jpg"  /> 
+      <img class="breakpoint"  src="images/aeon-jose-efsquPE1A1I-unsplash.jpg"     /> 
+      <img class="breakpoint"  src="images/david-dibert-Na0BbqKbfAo-unsplash.jpg"  /> 
+      <img class="breakpoint"  src="images/fabrice-villard-Jrl_UQcZqOc-unsplash.jpg"  />
+      <img class="breakpoint"  src="images/emma-frances-logan-Yypcm0DCJtQ-unsplash.jpg"  />
+      <img class="breakpoint"  src="images/florian-klauer-mk7D-4UCfmg-unsplash.jpg"  />
+      <img class="breakpoint"  src="images/trevin-rudy-Daq2QSqjfEo-unsplash.jpg"  /> 
+      <img class="breakpoint"  src="images/daniele-levis-pelusi-05Bimkui65I-unsplash.jpg"  />   
+      <img class="breakpoint"  src="images/zhu-liang-jRt4LXvGHlc-unsplash.jpg"  /> 
+     
+
+      
+    
+    </div>
+  </section> 
+  
+
+  <?php include 'pages\footer.php';?> 
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="backend/js/jquery.slim.min.js"></script>
+  <script src="backend/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ thoughts.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en"> 
+
+<?php include 'pages/header.php'; ?>  
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content=""> 
+  <link href="backend/bootstrap/css/main.css" rel="stylesheet"> 
+  <link href="backend/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<!-- Page Content --> 
+
+<section class="notes">
+  <div class="container">
+    Hi guys! This site was created just for you. <b>It's a very straight to the point site and simple</b>. You can <b>download</b> high quality pictures, poems, thoughts and sayings that are absolutely free(no copyright issues). Hope you like my monochromatic theme. Thanks!
+	</div>
+</section> 
+
+  <section id="responsive-images-columns">    
+    <?php $page = 'poetry'; include 'pages/mainheader.php';?>  
+    <hr>
+	  <div class="container"> 
+      <img class="breakpoint"  src="http://placehold.it/940x640" /> 
+      <img class="breakpoint"  src="http://placehold.it/940x640" /> 
+      <img class="breakpoint"  src="http://placehold.it/940x640" />  
+      <img class="breakpoint"  src="http://placehold.it/940x640" /> 
+      <img class="breakpoint"  src="http://placehold.it/940x640" />  
+      <img class="breakpoint"  src="http://placehold.it/940x640" /> 
+      <img class="breakpoint"  src="http://placehold.it/940x640" /> 
+      <img class="breakpoint"  src="http://placehold.it/940x640" />  
+      <img class="breakpoint"  src="http://placehold.it/940x640" /> 		
+     </div>
+  </section> 
+  
+  
+
+
+  <?php include 'pages\footer.php';?> 
+
+ 
+  <script src="backend/jquery/jquery.slim.min.js"></script>
+  <script src="backend/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
