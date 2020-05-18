2a10c364afd84698245ca6942abd5e94cafba039
plats16 <46740637+plats16@users.noreply.github.com>
2020-05-19 00:46:36 +0800

    Update load.php

--- load.php
+++ load.php
@@ -10,9 +10,9 @@ $data = '';
 while ($row = $result->fetch_assoc()){
     $data .= '<div class="col-lg-4 bg-light">
                 <figure class="figure">
-                    <img src="'.$row['image_path'].'" class="figure-img img-fluid rounded img-hover" height="300" alt="A generic square placeholder image with rounded corners in a figure."> 
+                    <img src="'.$row['IMAGE_PATH'].'" class="figure-img img-fluid rounded img-hover" height="300" alt="A generic square placeholder image with rounded corners in a figure."> 
                     <figcaption class="figure-caption text-center">
-                        <a href="'.$row['image_path'].'" class="text-center text-dark" download>Download</a>
+                        <a href="'.$row['IMAGE_PATH'].'" class="text-center text-dark" download>Download</a>
                     </figcaption>
                 </figure>
               </div>';
