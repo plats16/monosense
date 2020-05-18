73c8dab4d3e66dbf27c9200b3ad3b2cc32b8a173
plats16 <46740637+plats16@users.noreply.github.com>
2020-05-19 00:40:13 +0800

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,5 +1,5 @@
 <?php 
-$conn = new mysqli("localhost","root","","upload"); 
+$conn = new mysqli("qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","cc6n5y7f0z9ul5u4","lvns0umbcb5e4xhr","g05c6oo23imnkzld"); 
 
 foreach($_FILES['images']['name'] as $i => $value) {  
 
@@ -8,7 +8,7 @@ foreach($_FILES['images']['name'] as $i => $value) {
     $image_path = $folder.$_FILES['images']['name'][$i]; 
     move_uploaded_file($image_name,$image_path); 
 
-    $sql = "INSERT INTO images (image_path) VALUES (?)"; 
+    $sql = "INSERT INTO images (IMAGE_PATH) VALUES (?)"; 
     $stmt = $conn->prepare($sql); 
     $stmt->bind_param("s",$image_path);  
     $stmt->execute();
